68629bc127d1f6c1a9a1618dcf853f34be1f3ba3
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-06-16 11:12:41 +0200

    creating cards in section behandelingen

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -8054,7 +8054,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51640" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55488" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
