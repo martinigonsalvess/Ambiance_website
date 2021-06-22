65e28ec37fdc809d3da6deca665c7585e2639e57
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-06-22 15:05:00 +0200

    arrow up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import * as bootstrap from "bootstrap";
+
+const toTop = document.querySelector(".pijl-boven");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
